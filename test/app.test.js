e9065d753456a6a4c20f503358b6639c1b16c783
parker lindley <fugazi8@gmail.com>
2018-01-07 18:18:57 -0700

    Test App.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,29 +1,57 @@
-import React from 'react';
-import { mount, shallow } from 'enzyme';
-import App from '../lib/App.js';
-import mockData from '../lib/mock-data.js';
-import localStorageMock from '../__mocks__/localStorage';
-
-window.localStorage = localStorageMock;  
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import App from '../lib/App.js'
+import Search from '../lib/Search.js'
+import 'jest-localstorage-mock';
+import mockData from '../lib/mock-data.js'
 
 describe('App', () => {
-
   let wrapper;
-  beforeEach( () => {
+
+  beforeEach(() => {
     wrapper = shallow(<App />)
   })
 
   it('should exist', () => {
-    console.log(<App />)
-    expect(wrapper).toBeDefined();
+    expect(wrapper).toBeDefined()
   })
 
-  it('should render the elements on the page', () => {
-    localStorage.setItem('city', '80218');
-    expect(wrapper.find('Header').length).toEqual(1)
+  it('should have the correct initial state', () => {
+    expect(wrapper.state('apiData')).toEqual({})
+    expect(wrapper.state('error')).toEqual(false)
   })
 
-  it
+  it('should only render the Welcome component when nothing is in localStorage', () => {
+    expect(wrapper.find('Welcome').length).toEqual(1)
+    expect(localStorage.length).toBe(0); // or check length
+  })
 
+  it('should render the Error page when Apps state error is true or Apps response state exists', () => {
+    localStorage.setItem('city', 'hotdog')
+    expect(localStorage.setItem).toHaveBeenLastCalledWith('city', 'hotdog');
+    expect(localStorage.length).toBe(1);
+    expect(wrapper.find('ErrorPage').length).toEqual(1)
+    expect(wrapper.props().includedProp).toEqual(App.getData);
+  })
 
+  it('should render the App page when state has api data', () => {
+    localStorage.setItem('city', 'Eugene, OR')
+    expect(localStorage.setItem).toHaveBeenLastCalledWith('city', 'Eugene, OR');
+    expect(localStorage.length).toBe(1);
+    console.log("hi", wrapper.debug());
+    expect(wrapper.state().apiData).toEqual({})
+    expect(wrapper.find('div').length).toEqual(1);
+    expect(wrapper.find('div').children().length).toEqual(3);
+  })
+
+  it.only('should render Header, CurrentWeather, SevenHourForecast, and TenDayForecast if apiData is in this.state', () => {
+    localStorage.setItem('city', 'Santa Cruz, CA');
+
+    wrapper.setState( { apiData: mockData } );
+    expect(wrapper.find('Welcome').length).toEqual(0);
+    expect(wrapper.find('ErrorPage').length).toEqual(0);
+    expect(wrapper.find('Header').length).toEqual(1);
+    expect(wrapper.find('Current').length).toEqual(1);
+    expect(wrapper.find('Cards').length).toEqual(1);
+  });
 })
--- test/header.test.js
+++ test/header.test.js
@@ -1,2 +1,28 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import Header from '../lib/Header.js'
+import App from '../lib/App.js'
+import Search from '../lib/Search.js'
+import mockData from '../lib/mock-data.js'
+
+describe('Header', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = mount(<Header location={mockData.current_observation}
+                              getData={App.getData}
+                      />)
+    })
+
+    it('should exist', () => {
+      expect(wrapper).toBeDefined();
+    })
+
+    it('should start with props', () => {
+      expect(wrapper.props().location).toEqual(mockData.current_observation)
+    })
+
+    it('should mount one div, two h3s and search component', () => {
+      expect(wrapper.find('div').length).toEqual(1);
+    }) 
+
+})
